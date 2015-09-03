03adc94440caee4214472dad5e439efb57537a21
siozosdr <sokras2008@gmail.com>
2015-09-03 19:18:39 +0200

    added some stuff

--- Assignment 0/Curves.hs
+++ Assignment 0/Curves.hs
@@ -19,13 +19,36 @@ pointx :: Point -> Double
 
 pointy :: Point -> Double
 
+data Curve = Curve [Point]
 curve :: Point -> [Point] -> Curve
+curve p lp = Curve (p:lp)
 
 connect :: Curve -> Curve -> Curve
+connect (Curve c1) (Curve c2) = Curve (c1 ++ c2)
 
 rotate :: Curve -> Double -> Curve
+rotate (Curve []) _ = (Curve [])
+rotate (Curve (Point x y : pl)) deg = Curve (map(rotatePoint deg) pl)
+
+-- helper function for rotate
+-- rotates a single point based on a double d
+rotatePoint :: double -> Point -> Point
+rotatePoint d (Point x y) = (Point x' y')
+	where
+		r  = deg * pi / 180
+    	x' = x * cos r + y * sin r
+    	y' = y * cos r - x * sin r 
 
 translate :: Curve -> Point -> Curve
+translate (Curve []) _ = (Curve [])
+translate (Curve (Point x y: pl)) (Point px py) = Curve $ map (translatePoint tv) (Point x y: pl)
+	where 
+		--translation vector
+		tv = Point (px - x) (py - y)
+
+-- helper for translate
+translatePoint :: Point -> Point -> Point
+translatePoint (Point x y) (Point tx ty) = Point (x+tx) (y+ty)
 
 data Line = Vertical Double | Horizontal Double
 
